da528713981a820f515d2bf160ea5d8a0b5873cc
mto19 <mto19@HS-MATH2-26.gvsd.org>
2018-10-18 13:37:23 -0400

    hw

--- src/StringManipulatorMain.java
+++ src/StringManipulatorMain.java
@@ -3,7 +3,18 @@ public class StringManipulatorMain
 {
 	public static void main(String[] args)
 	{
-		String str = new noVowels();
-		
+		Scanner StrManip = new Scanner(System.in);
+		StringManipulator strIn = new StringManipulator();
+		System.out.println("Would you like to use noVowels or reverse: ");
+		String method = StrManip.nextLine();
+		Scanner stringIn = new Scanner(System.in);
+		System.out.println("Input your string: ");
+		String string = StrManip.nextLine();
+
+		if (method == "noVowels")
+		{
+			strIn.noVowels(string);
+		}
+		return strIn;
 	}
 }
